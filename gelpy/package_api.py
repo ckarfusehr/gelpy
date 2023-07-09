8ee2af6fad063abff380d08effeb20cf0772001d
karfusehr <c.karfusehr@tum.de>
2023-07-09 11:29:50 +0200

    refactor: extract functions

--- gelpy/package_api.py
+++ gelpy/package_api.py
@@ -4,64 +4,82 @@ from .profile_fitting_models import GaussianFitModel, EmgFitModel
 from .profile_fit_handling import LineFits
 from .background_ransac_fit_models import PlaneFit2d
 
+# Magic numbers and strings
+DEFAULT_GAMMA = 0.1
+DEFAULT_GAIN = 1
+DEFAULT_INTENSITY_RANGE = (0.05, 0.95)
+DEFAULT_IMG_HEIGHT_FACTOR = 0.005
+DEFAULT_LABEL_ROTATION = 45
+DEFAULT_SHOW_TYPE = "non_linear"
+MODEL_2D_PLANE_FIT_NAME = "2d_plane_fit"
+GAUSSIAN_FIT_NAME = "gaussian"
+EMG_FIT_NAME = "emg"
+
+
 class AgaroseGel:
     def __init__(self, path):
         self.path = path
-        
-    # def setup_gel(self, labels, x_label_pos, gamma=0.1, gain=1, intensity_range=(0.05, 0.95),
-    #                         img_height_factor=0.005, label_rotation=45, save=False, show_type="non_linear",
-    #                         line_profile_width=None):
-        
-    #     self.Image = Image(self.path, labels, x_label_pos, label_rotation,
-    #                        img_height_factor=img_height_factor, gamma=gamma, gain=gain, intensity_range=intensity_range)  
-        
-        
-    def setup_gel(self, labels, x_label_pos, gamma=0.1, gain=1, intensity_range=(0.05, 0.95),
-                             img_height_factor=0.005, label_rotation=45, save=False, show_type="non_linear",
-                             line_profile_width=None):
+        self.labels = None
+        self.x_label_positions = None
+        self.global_line_profile_width = None
+
+    def setup_gel(self, labels, x_label_pos, gamma=DEFAULT_GAMMA, gain=DEFAULT_GAIN, 
+                  intensity_range=DEFAULT_INTENSITY_RANGE, img_height_factor=DEFAULT_IMG_HEIGHT_FACTOR, 
+                  label_rotation=DEFAULT_LABEL_ROTATION, save=False, 
+                  show_type=DEFAULT_SHOW_TYPE, line_profile_width=None):
         """Has to be used as the first function after initiating the Gel"""
         
+        self.init_image(labels, x_label_pos, gamma, gain, intensity_range, img_height_factor, label_rotation)
+        self.plot_and_adjust_gels(show_type, save)
+        self.setup_line_profile(line_profile_width)
+
+    # Repeated function has been removed and instead init_image is reused
+    def show_adjusted_images(self, save=True, show_type="both"):
+        self.plot_and_adjust_gels(show_type, save)
+
+    def init_image(self, labels, x_label_pos, gamma, gain, intensity_range, img_height_factor, label_rotation):
         self.Image = Image(self.path, labels, x_label_pos, label_rotation,
                            img_height_factor=img_height_factor, gamma=gamma, gain=gain, intensity_range=intensity_range)
+        self.labels = labels
+        self.x_label_pos = x_label_pos 
+
+    def plot_and_adjust_gels(self, show_type, save):
         self.x_label_positions = self.Image.plot_adjusted_gels(show_type, save)
-        self.x_label_pos = x_label_pos # A workaround. Instead extrcat positions calculation from plotting function
-        self.labels = labels # rather use self.Image.labels and self.Image.x_label_pos?
         
-        # Utility functions for setting up gel well.
+    def setup_line_profile(self, line_profile_width):
         self.global_line_profile_width = LineProfiles.guess_line_profile_width(self.x_label_positions, self.Image.gel_image, line_profile_width)
         self.Image.color_line_profile_area(self.global_line_profile_width, color="r")
-        return
-    
-    def remove_background(self, model="2d_plane_fit", model_input=None):
-        if model == "2d_plane_fit":
+
+    def remove_background(self, model=MODEL_2D_PLANE_FIT_NAME, model_input=None):
+        self.init_background_model(model, model_input)
+        self.apply_background_model()
+
+    def init_background_model(self, model, model_input):
+        if model == MODEL_2D_PLANE_FIT_NAME:
             self.background_model = PlaneFit2d(self.Image.gel_image, model_input)
         else:
-            print("no valid model selected")
-            
+            print("No valid model selected")
+    
+    def apply_background_model(self):
         self.background_model.extract_fit_data_from_image()
         self.background_model.fit_model_to_data()
         self.Image.gel_image = self.background_model.substract_background()  # this sets the original gel_image to the bg corrected image
         self.background_model.visualize_fit_data()
-        
-    # repeated code with setup function..
-    def show_adjusted_images(self, gamma=0.1, gain=1, intensity_range=(0.05, 0.95),
-                             img_height_factor=0.009, label_rotation=45, save=False, show_type="both"):
-        image_for_display = Image(self.path, self.labels, self.x_label_pos, label_rotation, #extract the passing of x_label_pos here?
-                           img_height_factor=img_height_factor, gamma=gamma, gain=gain, intensity_range=intensity_range)
-        
-        image_for_display.plot_adjusted_gels(show_type, save)
-        return
-    
+
     def show_raw_gel(self):
         self.Image.show_raw_image()
-        return
-    
-    # set line_profile width to be what was picted before, but to still be able to be changed by the user.
+
+    # The line profiles logic is separated into a different method
     def show_line_profiles(self, select_lanes="all", slice_line_profile_length=(0,-1),
                            fit=None, maxima_threshold=0.001, maxima_prominence=None, plot_fits=False,
                            normalization_type="area", save=False, save_name_overview="selected_line_profiles.svg",
                            save_name_fits="selected_fitted_profiles.svg"):
         
+        self.init_line_profiles(select_lanes, slice_line_profile_length, normalization_type,
+                                save, save_name_overview)
+        self.apply_line_profiles(fit, maxima_threshold, maxima_prominence, plot_fits, save, save_name_fits)
+
+    def init_line_profiles(self, select_lanes, slice_line_profile_length, normalization_type, save, save_name_overview):
         self.LineProfiles = LineProfiles(self.Image.gel_image, self.labels, self.x_label_positions,
                                          select_lanes, slice_line_profile_length, normalization_type,
                                          save, save_name_overview)
@@ -69,19 +87,21 @@ class AgaroseGel:
         self.LineProfiles.extract_line_profiles()
         self.LineProfiles.normalize_line_profiles()
         self.LineProfiles.plot_selected_line_profiles()
-        
-        if fit=="gaussian":
+
+    def apply_line_profiles(self, fit, maxima_threshold, maxima_prominence, plot_fits, save, save_name_fits):
+        if fit == GAUSSIAN_FIT_NAME:
             fit_model = GaussianFitModel
-        elif fit == "emg":
+        elif fit == EMG_FIT_NAME:
             fit_model = EmgFitModel
         else:
-            print(f"Invalid fit type")
+            print("Invalid fit type")
             return
-            
+        
         self.LineFits = LineFits(fit_model, self.LineProfiles.selected_line_profiles_normalized, self.LineProfiles.selected_labels,
-                                    maxima_threshold, maxima_prominence, save, save_name_fits)
+                                 maxima_threshold, maxima_prominence, save, save_name_fits)
         self.LineFits.fit()
         self.LineFits.display_dataframe()
         
-        if plot_fits == True:
+        if plot_fits:
             self.LineFits.plot_fits_and_profiles()
+
