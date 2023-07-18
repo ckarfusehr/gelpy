205a19e87472356dc1f44b0aee1d21c247851c7e
karfusehr <c.karfusehr@tum.de>
2023-07-18 17:22:56 +0200

    Update setup.py file based on .toml file.

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='gelpy',
-    version='0.1',
+    version='0.0.1',
     packages=find_packages(),
     author="Christoph Karfusehr",
-    author_email="c.kafusehr@tum.de",
+    author_email="c.karfusehr@gmail.com",
+    description="A small python package used for gel electrophoresis visualization, fitting and analysis",
     install_requires=[
-        "matplotlib",
-        "numpy",
-        "scipy",
-        "scikit-image",
-        "pandas",
-        "seaborn",
-        "Ipython",
-        "ipykernel",
+        "numpy>=1.24.3",
+        "scipy>=1.10.1",
+        "scikit-image>=0.21.0",
+        "scikit-learn>=1.2.2",
+        "matplotlib>=3.7.1",
+        "seaborn>=0.12.2",
+        "pandas>=2.0.2",
     ],
 )
