c9dd812616d9198134db9b4d335deb703e83293e
Christoph Karfusehr <c.karfusehr@tum.de>
2024-09-27 11:36:40 +0200

    fix warning

--- src/gelpy/__init__.py
+++ src/gelpy/__init__.py
@@ -3,5 +3,5 @@ import warnings
 
 warnings.warn(
     "gelpy is deprecated and no longer maintained. It may not work with current versions of Python or its dependencies.",
-    DeprecationWarning,
+    UserWarning,
 )
